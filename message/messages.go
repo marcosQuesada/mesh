93ea74b2d0a009e7bb24bd3ea182b8d81648bc32
marcosQuesada <marcos.quesadas@gmail.com>
2015-06-07 19:40:55 +0200

    add origin & destination on all messages

--- message/messages.go
+++ message/messages.go
@@ -6,6 +6,8 @@ import (
 
 type Message interface {
 	MessageType() MsgType
+	Origin() n.Node
+	Destination() n.Node
 }
 
 type MsgType int
@@ -13,7 +15,7 @@ type MsgType int
 func (mt MsgType) New() Message {
 	switch mt {
 	case HELLO:
-		return &Hello{From: n.Node{}}
+		return &Hello{} //from: n.Node{}
 	case WELCOME:
 		return &Welcome{}
 	case ABORT:
@@ -45,6 +47,7 @@ const (
 type Hello struct {
 	Id      int
 	From    n.Node
+	To      n.Node
 	Details map[string]interface{}
 }
 
@@ -52,10 +55,19 @@ func (h Hello) MessageType() MsgType {
 	return HELLO
 }
 
+func (h Hello) Origin() n.Node {
+	return h.From
+}
+
+func (h Hello) Destination() n.Node {
+	return h.To
+}
+
 // Hello Accepted
 type Welcome struct {
 	Id      int
 	From    n.Node
+	To      n.Node
 	Details map[string]interface{}
 }
 
@@ -63,10 +75,19 @@ func (w Welcome) MessageType() MsgType {
 	return WELCOME
 }
 
+func (h Welcome) Origin() n.Node {
+	return h.From
+}
+
+func (h Welcome) Destination() n.Node {
+	return h.To
+}
+
 // Hello Rejected
 type Abort struct {
 	Id      int
 	From    n.Node
+	To      n.Node
 	Details map[string]interface{}
 }
 
@@ -74,10 +95,19 @@ func (a Abort) MessageType() MsgType {
 	return ABORT
 }
 
+func (h Abort) Origin() n.Node {
+	return h.From
+}
+
+func (h Abort) Destination() n.Node {
+	return h.To
+}
+
 // Ping request to a remote node
 type Ping struct {
 	Id      int
 	From    n.Node
+	To      n.Node
 	Details map[string]interface{}
 }
 
@@ -85,10 +115,19 @@ func (p Ping) MessageType() MsgType {
 	return PING
 }
 
+func (h Ping) Origin() n.Node {
+	return h.From
+}
+
+func (h Ping) Destination() n.Node {
+	return h.To
+}
+
 // Pong response as a ping request
 type Pong struct {
 	Id      int
 	From    n.Node
+	To      n.Node
 	Details map[string]interface{}
 }
 
@@ -96,9 +135,18 @@ func (p Pong) MessageType() MsgType {
 	return PONG
 }
 
+func (h Pong) Origin() n.Node {
+	return h.From
+}
+
+func (h Pong) Destination() n.Node {
+	return h.To
+}
+
 type GoodBye struct {
 	Id      int
 	From    n.Node
+	To      n.Node
 	Details map[string]interface{}
 }
 
@@ -106,14 +154,31 @@ func (g GoodBye) MessageType() MsgType {
 	return GOODBYE
 }
 
+func (h GoodBye) Origin() n.Node {
+	return h.From
+}
+
+func (h GoodBye) Destination() n.Node {
+	return h.To
+}
+
 type Error struct {
 	Id      int
 	From    n.Node
+	To      n.Node
 	Details map[string]interface{}
 }
 
 func (w Error) MessageType() MsgType {
 	return ERROR
+}
+
+func (h Error) Origin() n.Node {
+	return h.From
+}
+
+func (h Error) Destination() n.Node {
+	return h.To
 }
 
 type Status string
